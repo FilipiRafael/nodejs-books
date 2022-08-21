b9b89c293cce935107d6505497daf97341625e6a
Filipi Rafael <filipirafael.123@gmail.com>
2022-08-21 18:43:11 -0300

    first commig

---
+++ database.js
@@ -0,0 +1,59 @@
+
+const books = [
+    {
+        id: 1,
+        nome: 'Digital Minimalism',
+        autor: 'Call Newport',
+        categoria: 'Produtividade e estilo de vida',
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 2,
+        nome: 'The minimalism',
+        autor: 'Call Newport',
+        categoria: 'Produtividade e estilo de vida',
+        paginas: 320,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 3,
+        nome: 'Digital Market',
+        autor: 'Call Newport',
+        categoria: 'Produtividade e estilo de vida',
+        paginas: 148,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 4,
+        nome: 'Harry Potter',
+        autor: 'Call Newport',
+        categoria: 'Produtividade e estilo de vida',
+        paginas: 392,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 5,
+        nome: 'Digital Minimalism',
+        autor: 'Call Newport',
+        categoria: 'Produtividade e estilo de vida',
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 6,
+        nome: 'Digital Minimalism',
+        autor: 'Call Newport',
+        categoria: 'Produtividade e estilo de vida',
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    },
+]
+
+module.exports = books;
